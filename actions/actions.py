995ef4080617ee47096b4fb7e553f3e9c6da58b2
vleroy005 <58265517+vleroy005@users.noreply.github.com>
2021-12-09 19:10:09 +0100

    Init country_form

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,44 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Text, List, Any, Dict
+
+from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ValidateCountryForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_country_form"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    @staticmethod
+    def country_db() -> List[Text]:
+        #Here we want to return all the county names of the API
+        
+        return []
+
+    def validate_country(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate country value."""
+
+        if slot_value.lower() in self.country_db():
+            # validation succeeded, set the value of the "country" slot to value
+            return {"country": slot_value}
+        else:
+            # validation failed, set this slot to None so that the
+            # user will be asked for the slot again
+            return {"country": None}
+
+class AskForSlotAction(Action):
+    #custom ask for slot
+    def name(self) -> Text:
+        return "action_ask_country"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        dispatcher.utter_message(text="Which country do you want to visit?")
+        return []
