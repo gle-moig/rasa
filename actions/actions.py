7eadfd947aaaa44630f612c84ff819d228649790
Le Moigne <glemoigne@ensc.fr>
2021-12-11 10:03:54 +0000

    doesnt validate slot informations...

--- actions/actions.py
+++ actions/actions.py
@@ -19,10 +19,12 @@ class ValidateCountryForm(FormValidationAction):
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        response = request.get(f"{API}/{slot_value.lower()}")
+        print(slot_value)
+        response = requests.get(f"{API}/name/{slot_value.lower()}")
         if response.status_code == 200:
+            print(response.json())
             if len(response.json()) == 1:
-                return {"country": response.json()["name"]["common"]}
+                return {"country": response.json()[0]["name"]["common"]}
             else:
                 dispatcher.utter_message(f"Please be more precise, i got multiple results for {slot_value}")
         return {"country": None}
@@ -36,7 +38,7 @@ class ValidateCountryForm(FormValidationAction):
     ) -> Dict[Text, Any]:
         intent = tracker.get_intent_of_latest_message()
         country = tracker.get_slot("country")
-        response = request.get(f"{API}/{country}")
+        response = requests.get(f"{API}/name/{country}")
         if intent == "everything":
             return {"informations": response.json()[0]}
         if slot_value.lower() in response.json()[0]:
